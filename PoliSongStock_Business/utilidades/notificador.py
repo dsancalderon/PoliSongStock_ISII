23fda6a193272b0b9ce26628ecdb796546815016
Alejandro Gonzalez <alej02992>
2025-11-24 13:36:29 -0500

    Actualizacion Utilidades Notificador

---
+++ PoliSongStock_Business/utilidades/notificador.py
@@ -0,0 +1,4 @@
+def enviar_correo_simulado(destinatario:str, asunto:str, cuerpo:str):
+    # Simula envío de correo (solo print)
+    print(f"[SIMULADO] Enviando correo a {destinatario} — Asunto: {asunto}")
+    print(cuerpo)
